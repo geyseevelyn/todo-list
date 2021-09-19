5b63bf0aa58dd09b3b208a974e475ed7edea6285
Geyse Evelyn <geyseevelynod@gmail.com>
2021-09-19 17:50:24 -0300

    Criação do projeto Spring

---
+++ backend/src/main/java/br/edu/ufersa/pw/todolist/TodolistApplication.java
@@ -0,0 +1,20 @@
+package br.edu.ufersa.pw.todolist;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistApplication {
+	
+	private static final Logger logger =
+		LoggerFactory.getLogger(TodolistApplication.class);
+			
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistApplication.class, args);
+		logger.info("Hello start");
+	}
+
+}
