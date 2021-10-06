20c86a3ee2f1d9b23362c858b3e8ee245f6fae7a
Geyse Evelyn <geyseevelynod@gmail.com>
2021-10-06 01:58:38 -0300

    Criação do ToDoList com React

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import './App.css';
+import data from './data.json';
+
+import Header from './components/Header';
+import ToDoList from './components/ToDoList';
+
+function App() {
+
+  const [toDoList, setToDoList] = useState(data);
+
+  const handleToggle = (id) => {
+    let mapped = toDoList.map(task => {
+
+      return task.id === id ? { ...task, complete: !task.complete } : { ...task };
+
+    });
+    setToDoList(mapped);
+  }
+
+  return (
+    <div className="App">
+      <Header />
+      <ToDoList toDoList={toDoList} handleToggle={handleToggle} />
+    </div>
+  );
+}
+
+export default App;
