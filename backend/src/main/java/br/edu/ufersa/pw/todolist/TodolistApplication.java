ee9aba6f654b6db9a263e1668c9751db2b04baec
Geyse Evelyn <geyseevelynod@gmail.com>
2021-10-06 02:05:27 -0300

    Modificação no pom.xml

--- backend/src/main/java/br/edu/ufersa/pw/todolist/TodolistApplication.java
+++ backend/src/main/java/br/edu/ufersa/pw/todolist/TodolistApplication.java
@@ -1,16 +1,10 @@
 package br.edu.ufersa.pw.todolist;
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import br.edu.ufersa.pw.todolist.entity.User;
 import br.edu.ufersa.pw.todolist.repositories.UserRepository;
 
 @SpringBootApplication
@@ -26,13 +20,5 @@ public class TodolistApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistApplication.class, args);
 		logger.info("Hello start");
-	}
-	
-	@Bean
-	CommandLineRunner runner() {
-		return args -> {
-			 List<User> users = userRepo.findAll();
-			 users.forEach(user -> System.out.println(user.getEmail()));
-		};
 	}
 }
