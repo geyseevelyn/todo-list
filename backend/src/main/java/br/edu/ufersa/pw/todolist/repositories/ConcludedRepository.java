1cd989b1e144037042d88a7110ff703e1823d58c
Geyse Evelyn <geyseevelynod@gmail.com>
2021-10-19 12:10:58 -0300

    Criação das classes ConcludedRepository e ConcludedService

--- backend/src/main/java/br/edu/ufersa/pw/todolist/repositories/ConcludedRepository.java
+++ backend/src/main/java/br/edu/ufersa/pw/todolist/repositories/ConcludedRepository.java
@@ -1,9 +1,28 @@
 package br.edu.ufersa.pw.todolist.repositories;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import br.edu.ufersa.pw.todolist.entities.Concluded;
 
 public interface ConcludedRepository extends JpaRepository<Concluded,Long>{
-
+	
+	@RestResource(exported = false)
+	@Query(
+			value = "SELECT t.id, t.id_user, t.todo, t.completed_in FROM tb_concluded t "
+					+ "INNER JOIN tb_users user ON user.id=t.id_user AND user.email= :email",
+					nativeQuery = true)
+	List<Concluded> findByUser(String email);
+	
+	@RestResource(exported = false)
+	@Query(
+			value = "SELECT t.id, t.id_user, t.todo, t.completed_in FROM tb_concluded t "
+					+ "INNER JOIN tb_users user ON user.id=t.id_user AND user.email= :email "
+					+ "AND t.deadline= :deadline",
+					nativeQuery = true)
+	List<Concluded> findByUserAndDeadline(String email, LocalDate deadline);
 }
---
+++ backend/src/main/java/br/edu/ufersa/pw/todolist/services/ConcludedService.java
@@ -0,0 +1,33 @@
+package br.edu.ufersa.pw.todolist.services;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.edu.ufersa.pw.todolist.dto.TodoDto;
+import br.edu.ufersa.pw.todolist.entities.Concluded;
+import br.edu.ufersa.pw.todolist.repositories.ConcludedRepository;
+
+@Service
+public class ConcludedService {
+
+	@Autowired
+	private ConcludedRepository repo;
+	
+	@Transactional(propagation=Propagation.REQUIRED, readOnly=false)
+	public List<TodoDto> findByUser(String email){
+		List<Concluded> todos = repo.findByUser(email);
+		return todos.stream().map(x -> new TodoDto(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional(propagation=Propagation.REQUIRED, readOnly=false)
+	public List<TodoDto> findByUserAndDeadline(String email, LocalDate deadline){
+		List<Concluded> todos = repo.findByUserAndDeadline(email, deadline);
+		return todos.stream().map(x -> new TodoDto(x)).collect(Collectors.toList());
+	}
+}
